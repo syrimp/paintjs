ca8bba6925d721f31ebf64cb3a13bcfccbad43ea
syrimp <rimyesong@naver.com>
2021-12-01 00:26:22 +0900

    완료!!!

--- app.js
+++ app.js
@@ -3,14 +3,24 @@ const ctx = canvas.getContext("2d"); //2d의 context를 가지게됨
 const colors = document.getElementsByClassName("jsColors");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jssave");
 //캔버스에 그림을 그리려면 css로 정의된 캔버스 사이즈 말고 
 //js에서도 pixel modifier에 canvas의 픽셀 사이즈를 정의해야 한다.
-canvas.width = 700;
-canvas.height = 700;
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
 
-ctx.strokeStyle = "#2c2c2c"; //선색
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR; //선색
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5; //선 굵기
+// ctx.fillRect(50,20,100,40); //채우기 좌표, 크기
+
+
 
 let painting = false;
 let filling = false;
@@ -37,6 +47,7 @@ function onMouseMove(event){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 function handleRangeChange(event){
     const size = event.target.value;
@@ -52,11 +63,30 @@ function handleModeClick(){
     }
 }
 
+function handleCanvasClick(){
+    if(filling){
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+}
+
+function handleRightClick(event){
+    event.preventDefault();
+}
+function handleSaveClick(){
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[♩]";
+    link.click();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleRightClick)
 }
 
 Array.from(colors).forEach(color=>color.addEventListener("click", handleColorClick));
@@ -66,4 +96,7 @@ if(range){
 }
 if(mode){
     mode.addEventListener("click", handleModeClick);
+}
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
